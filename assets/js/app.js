624674a82110532f7e63aed60cfb32da3417cfcd
Bernadette <bernadette@englemaninvest.com>
2019-01-06 13:12:25 -0500

    Add menu bar template

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,11 @@
 (function(){
     var app = angular.module('website', []);
 
-    app.controller('MenuController', ['$scope', '$http', function(scope, http){
-
-        http.get('data/menu.json').success(function(data){
-            scope.menu = data;
+    // Office Hours
+    app.controller('ScheduleController', ['$scope', '$http', function(scope, http){
+        http.get('data/office-hours.json').success(function(data){
+            scope.schedule = data;
         });
-
     }]);
 
     // Home Page Featured Services
@@ -18,7 +17,62 @@
         this.isSelected = function(checkTab){
             return this.tab === checkTab;
         };
+    });
 
+    // Team Page
+    app.controller('TeamController', ['$scope', '$http', function(scope, http){
+        http.get('data/about-Team.json').success(function(data){
+            scope.photos = data;
+        });
+    }]);
+
+    // Gallery Page
+    app.controller('GalleryController', ['$scope', '$http', function(scope, http){
+        http.get('data/galleryPage-Photos.json').success(function(data){
+            scope.photos = data;
+        });
+    }]);
+
+    // Patients Page Forms
+    app.controller('PatientFormsController', ['$scope', '$http', function(scope, http){
+        http.get('data/patientsPage-Forms.json').success(function(data){
+            scope.forms = data;
+        });
+    }]);
+
+    app.directive('menuBar', function(){
+        return {
+            restrict: 'A',
+            template:
+                '<div class="place-nav">' +
+                    '<a title="Fortino logo" class="logo" href="http://www.fortinodental.com/">' +
+                        '<img alt="Fortino Dental logo" src="assets/img/logo-fortino-dental2.jpg"/>' +
+                    '</a>' +
+
+                    '<nav id="desktop-nav">' +
+                        '<ul>' +
+                            '<li data-ng-repeat="menu in menu">' +
+                                '<a href="{{menu.link}}" title="{{menu.name}}">{{menu.name}}</a>' +
+                                '<ul>' +
+                                    '<li data-ng-repeat="page in menu.pages">' +
+                                        '<a href="{{page.link}}" title="{{page.name}}">{{page.name}}</a>' +
+                                    '</li>' +
+                                '</ul>' +
+                            '</li>' +
+                        '</ul>' +
+                    '</nav>' +
+                '</div>',
+            controller: ['$scope', '$http', function(scope, http){
+                http.get('data/menu.json').success(function(data){
+                    scope.menu = data;
+                });
+            }],
+            controllerAs: 'menu'
+
+        };
     });
 
 })();
+
+
+
