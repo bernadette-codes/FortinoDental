7ff9cef991514e4dfee3f009639e7c3eecad3ff2
Bernadette <bernadette@englemaninvest.com>
2019-01-06 14:50:49 -0500

    move directives to dependencies

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 (function(){
-    var app = angular.module('website', []);
+    var app = angular.module('website', ['header-menu', 'footer-section']);
 
     // Office Hours
     app.controller('ScheduleController', ['$scope', '$http', function(scope, http){
@@ -40,39 +40,4 @@
         });
     }]);
 
-    app.directive('menuBar', function(){
-        return {
-            restrict: 'A',
-            template:
-                '<div class="place-nav">' +
-                    '<a title="Fortino logo" class="logo" href="http://www.fortinodental.com/">' +
-                        '<img alt="Fortino Dental logo" src="assets/img/logo-fortino-dental2.jpg"/>' +
-                    '</a>' +
-
-                    '<nav id="desktop-nav">' +
-                        '<ul>' +
-                            '<li data-ng-repeat="menu in menu">' +
-                                '<a href="{{menu.link}}" title="{{menu.name}}">{{menu.name}}</a>' +
-                                '<ul>' +
-                                    '<li data-ng-repeat="page in menu.pages">' +
-                                        '<a href="{{page.link}}" title="{{page.name}}">{{page.name}}</a>' +
-                                    '</li>' +
-                                '</ul>' +
-                            '</li>' +
-                        '</ul>' +
-                    '</nav>' +
-                '</div>',
-            controller: ['$scope', '$http', function(scope, http){
-                http.get('data/menu.json').success(function(data){
-                    scope.menu = data;
-                });
-            }],
-            controllerAs: 'menu'
-
-        };
-    });
-
 })();
-
-
-
